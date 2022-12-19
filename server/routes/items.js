348f0039badebe5ba6ba69d10915c48c44a1d246
Uri Nemirovsky <urinemirovsky@yahoo.com>
2022-12-19 13:40:05 -0500

    put and delete routes for item model

--- server/routes/items.js
+++ server/routes/items.js
@@ -12,4 +12,64 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.put("/:id", [
+  check("name").isString(),
+  check("description").isString(),
+  check("price").isFloat({min: 0}),
+  check("category").isString(),
+  check("image").isString()
+], async (req, res) => {
+  try {
+    const itemToChange = await Item.findByPk(req.params.id);
+    const updatedItem = await itemToChange.update({
+      name: req.params.name,
+      description: req.params.description,
+      price: req.params.price,
+      category: req.params.category,
+      image: req.params.image
+    })
+    res.json(updatedItem);
+  } catch (error) {
+    res.json(error);
+  }
+})
+
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const itemToDelete = await Item.findByPk(req.params.id);
+    const deletedItem = await itemToDelete.destroy()
+    res.json(deletedItem);
+  } catch (error) {
+    res.json(error);
+  }
+})
+
 module.exports = router;
